99a0c468685fae513ec032ebe90fda8a25e7c788
Kaden-Beck <kbeck1@byupathway.edu>
2025-07-07 21:52:59 -0600

    Finishing Week 02 Team Assignment to match repository

--- src/js/product.js
+++ src/js/product.js
@@ -1,25 +1,20 @@
+import { getParam } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
-import ProductData from './ProductData.mjs';
-import { getParam } from './utils.mjs';
-import ProductDetails from './ProductDetails.mjs';
+const dataSource = new ProductData("tents");
+const productID = getParam("product");
 
-
-const dataSource = new ProductData('tents');
-const productId = getParam('product');
-
-const product = new ProductDetails(productId, dataSource);
+const product = new ProductDetails(productID, dataSource);
 product.init();
-// console.log(dataSource.findProductById(productId));
 
+// // add to cart button event handler
+// async function addToCartHandler(e) {
+//   const product = await dataSource.findProductById(e.target.dataset.id);
+//   addProductToCart(product);
+// }
 
-
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+// // add listener to Add to Cart button
+// document
+//   .getElementById("addToCart")
+//   .addEventListener("click", addToCartHandler);
