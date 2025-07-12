a642468d87097c2b884f69f61e2eb222cadfd989
clane83 <clane83@yahoo.com>
2025-07-12 10:40:20 -0700

    update cart total

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,10 +1,24 @@
 import { getLocalStorage } from './utils.mjs';
 
+
+
+
 function renderCartContents() {
-  const cartItems = getLocalStorage('so-cart');
+  const cartItems = getLocalStorage('so-cart') || [];
+
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector('.product-list').innerHTML = htmlItems.join('');
+
+  const cartTotal = cartItems.reduce(
+    (sum, item) => sum + item.FinalPrice * (item.quantity || 1),
+    0
+  );
+
+  document.getElementById('cart-total').innerText = `Total: $${cartTotal.toFixed(2)}`;
 }
+
+
+
 
 function cartItemTemplate(item) {
   const newItem = `<li class='cart-card divider'>
@@ -14,7 +28,7 @@ function cartItemTemplate(item) {
       alt='${item.Name}'
     />
   </a>
-  <a href='#'>
+  <a href='/product_pages/?product=${item.Id}'>
     <h2 class='card__name'>${item.Name}</h2>
   </a>
   <p class='cart-card__color'>${item.Colors[0].ColorName}</p>
