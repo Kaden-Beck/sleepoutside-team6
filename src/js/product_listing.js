12cab0f7e937f09947abb05e0350d0cece64dca2
Kaden Beck <kbeck1@byupathway.edu>
2025-07-15 16:31:33 -0600

    Product Pages Working, need to fix cards and detail pages

--- src/js/product_listing.js
+++ src/js/product_listing.js
@@ -1,15 +1,19 @@
-import { loadHeaderFooter } from './utils.mjs';
 import ProductData from './ProductData.mjs';
 import ProductList from './ProductList.mjs';
 import Alert from './Alert';
+import { loadHeaderFooter, getParam } from './utils.mjs';
 
 loadHeaderFooter();
 
-const dataSource = new ProductData('tents');
-const element = document.querySelector('.product-list');
-const productList = new ProductList('tents', dataSource, element);
+const category = getParam('category');
 
-productList.init();
+const dataSource = new ProductData();
+
+const listElement = document.querySelector('.product-list');
+
+const myList = new ProductList(category, dataSource, listElement);
+
+myList.init();
 
 const myAlert = new Alert('../public/json/alerts.json');
 myAlert.init();
