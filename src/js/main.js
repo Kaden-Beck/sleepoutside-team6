0dfe3502f6e8c0899f17d10ba30c92e99feefde3
clane83 <clane83@yahoo.com>
2025-07-22 21:52:38 -0700

    accessing thankyou.html page

--- src/js/main.js
+++ src/js/main.js
@@ -27,7 +27,7 @@ if (newsletterForm) {
 
         if (email !== '') {
             localStorage.setItem('newsletterEmail', email);
-            window.location.href = '../thankyou.html';
+            window.location.href = '/sleepoutside-team6/src/thankyou.html';
         } else {
             alert('Please enter a valid email address.');
         }
